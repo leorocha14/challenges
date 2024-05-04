2714b648aeffd410d278851db9771fc63d5702cd
Leo <leonardorochabarros@hotmail.com>
2024-05-04 15:29:25 -0300

    Adding public access modifier to access the props

--- addTwoNumbers/src/main/java/com/add/two/numbers/models/ListNode.java
+++ addTwoNumbers/src/main/java/com/add/two/numbers/models/ListNode.java
@@ -14,8 +14,8 @@ import java.util.Objects;
  */
 
 public class ListNode {
-     int val;
-     ListNode next;
+     public int val;
+     public ListNode next;
      ListNode() {}
      public ListNode(int val) { this.val = val; }
      public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
