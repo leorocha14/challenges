07169dd8d05b4b99a364dd7b2c22a41a93cfecd0
Leo <leonardorochabarros@hotmail.com>
2024-05-04 16:36:14 -0300

    First solution for add two numbers problem - without code improvements

--- addTwoNumbers/src/main/java/com/add/two/numbers/Solution.java
+++ addTwoNumbers/src/main/java/com/add/two/numbers/Solution.java
@@ -2,8 +2,57 @@ package com.add.two.numbers;
 
 import com.add.two.numbers.models.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode(1);
+
+
+        List<Integer> nodes = new ArrayList<>();
+        nodes.add(l1.val);
+        ListNode currentNode = l1.next;
+        while (currentNode != null) {
+            nodes.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        List<Integer> reversedNodes = nodes.reversed();
+
+        StringBuffer sb = new StringBuffer();
+        reversedNodes.forEach(sb::append);
+
+        Integer firstNumber = Integer.valueOf(sb.toString());
+
+        nodes.clear();
+
+        nodes.add(l2.val);
+        currentNode = l2.next;
+        while (currentNode != null) {
+            nodes.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        reversedNodes = nodes.reversed();
+
+        sb = new StringBuffer();
+        reversedNodes.forEach(sb::append);
+
+        Integer secondNumber = Integer.valueOf(sb.toString());
+
+        int sumOfTheNumbers = firstNumber + secondNumber;
+
+        char[] sumAsCharArray = String.valueOf(sumOfTheNumbers).toCharArray();
+
+        ListNode sumAsListNode = new ListNode(Integer.parseInt(String.valueOf(sumAsCharArray[sumAsCharArray.length - 1])));
+
+        currentNode = sumAsListNode;
+        for (int i = sumAsCharArray.length - 2; i >= 0; i--) {
+            currentNode.next = new ListNode();
+            currentNode = currentNode.next;
+            currentNode.val = Integer.parseInt(String.valueOf(sumAsCharArray[i]));
+        }
+
+        return sumAsListNode;
     }
 }
